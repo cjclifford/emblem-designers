865a5d00408466569770dcb0b658ccbbc3de5385
cjclifford <cliffordcuan@gmail.com>
2020-07-10 18:45:20 +0200

    Adds user page and static data script

---
+++ js/designer.js
@@ -0,0 +1,47 @@
+(function() {
+  console.log('designer');
+  teams = [
+    {
+      teamName: 'team-africa',
+      teamMembers: [
+        {
+          fullName: 'Name Surname',
+          memberImage: './images/placeholder.svg'
+        }
+      ],
+      designImage: './images/placeholder.svg',
+      designRationale: `Sed sed tristique tellus. Pellentesque semper magna
+        dictum enim congue, vel fermentum eros dictum. Sed quis leo justo. Proin rhoncus varius
+        tempus. Aliquam et fermentum est. Praesent diam justo, aliquet id ligula et, dignissim
+        dictum quam. Mauris posuere lorem in arcu auctor, sit amet dignissim mauris dignissim. Nam
+        tempor nulla lorem, nec pellentesque enim elementum eget. Maecenas placerat consequat erat
+        sed congue. Nulla vitae posuere arcu. Nulla et lorem feugiat arcu auctor vehicula nec et
+        metus.`
+    }
+  ];
+
+  const params = new URLSearchParams(window.location.search);
+  let selectedTeam;
+  let selectedTeamMember;
+
+  
+  for (let team of teams) {
+    if (team.teamName = params.get('team')) {
+      selectedTeam = team;
+      break;
+    }
+  }
+
+  for (let teamMember of selectedTeam.teamMembers) {
+    if (teamMember.fullName = params.get('member')) {
+      selectedTeamMember = teamMember;
+      break;
+    }
+  }
+
+  document.getElementById('member-name').innerText = selectedTeamMember.fullName;
+  document.getElementById('member-team').innerText = selectedTeam.teamName;
+  document.getElementById('member-image').src = selectedTeamMember.memberImage;
+  document.getElementById('member-design').src = selectedTeam.designImage;
+  document.getElementById('member-design-rationale').innerText = selectedTeam.designRationale;
+})();
